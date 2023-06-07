2e6a030d020c4e21303641aa2319bb9f158abcd6
박철호 <chpark@bagcheolhoui-iMac.local>
2023-06-07 14:28:22 +0900

    first commit

---
+++ src/main/java/com/chpark/example/githubactiontest/GithubActionTestApplication.java
@@ -0,0 +1,13 @@
+package com.chpark.example.githubactiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionTestApplication.class, args);
+    }
+
+}
